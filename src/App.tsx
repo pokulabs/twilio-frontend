0383c9357b31d2762b1ef645288c65fca356f839
nickstauffer21 <nickstauffer456@gmail.com>
2025-10-29 19:14:19 -0400

    joy/material ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { CssVarsProvider } from "@mui/joy/styles";
-import { CssBaseline, Box } from "@mui/joy";
+import { createTheme, ThemeProvider, THEME_ID as MATERIAL_THEME } from "@mui/material/styles";
+import {CssBaseline, Box} from "@mui/material";
+
+const materialTheme = createTheme();
 
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Messages/Header";
@@ -19,29 +22,31 @@ export default function App() {
   return (
     <TwilioProvider>
       <WebsocketProvider>
-        <CssVarsProvider disableTransitionOnChange>
-          <CssBaseline />
-          <Router>
-            <Box
-              sx={{
-                display: "flex",
-              }}
-            >
-              <Sidebar />
-              <Header />
-              <Routes>
-                <Route path="/messages" element={<Messages />} />
-                <Route path="/campaigns" element={<Campaigns />} />
-                <Route path="/flagging" element={<Flagging />} />
-                <Route path="/integrations" element={<Integrations />} />
-                <Route path="/" element={<Account />} />
-                <Route path="/hitl" element={<Hitl />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/admin" element={<Admin />} />
-              </Routes>
-            </Box>
-          </Router>
-        </CssVarsProvider>
+        <ThemeProvider theme={{[MATERIAL_THEME]: materialTheme}}>
+          <CssVarsProvider disableTransitionOnChange>
+            <CssBaseline />
+            <Router>
+              <Box
+                sx={{
+                  display: "flex",
+                }}
+                >
+                <Sidebar />
+                <Header />
+                <Routes>
+                  <Route path="/messages" element={<Messages />} />
+                  <Route path="/campaigns" element={<Campaigns />} />
+                  <Route path="/flagging" element={<Flagging />} />
+                  <Route path="/integrations" element={<Integrations />} />
+                  <Route path="/" element={<Account />} />
+                  <Route path="/hitl" element={<Hitl />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/admin" element={<Admin />} />
+                </Routes>
+              </Box>
+            </Router>
+          </CssVarsProvider>
+        </ThemeProvider>
       </WebsocketProvider>
     </TwilioProvider>
   );
